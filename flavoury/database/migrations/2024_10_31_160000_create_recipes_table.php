885d3d5adcc18eb465a0544e933936a7b4e6fc4b
AufaRZ05 <aufa.rzp10@gmail.com>
2024-11-03 17:11:13 +0700

    Update DB Part 1

--- flavoury/database/migrations/2024_10_31_160000_create_recipes_table.php
+++ flavoury/database/migrations/2024_10_31_160000_create_recipes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('recipes', function (Blueprint $table) {
             $table->id("id_recipe");
             $table->unsignedBigInteger("id_user");
+            $table->string("name_recipe", 100);
             $table->char("descrip_recipe", 250);
             $table->char("ingredient", 250);
             $table->char("location", 100);
